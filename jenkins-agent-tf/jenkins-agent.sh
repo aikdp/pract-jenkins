f1e3f6056394c8717835e33432cce89ae0d650a9
aikdp <kdprasad028@gmail.com>
2025-03-19 22:53:56 +0530

    Jenkins CICD practice

--- jenkins-agent-tf/jenkins-agent.sh
+++ jenkins-agent-tf/jenkins-agent.sh
@@ -1,4 +1,61 @@
 #!/bin/bash
 
-sudo yum install java-17-openjdk -y
+USER_ID=$(id -u)
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is FAILED"
+        exit 1
+    else
+        echo "$2 is SUCCESS"
+     fi
+}
+
+CHECK(){
+    if [ $USER_ID -ne 0 ]
+    then 
+        echo "Please Run this scirpt with ROOT previleges"
+        exit 1
+    fi
+}
+CHECK
+
+#java
+sudo yum install java-17-openjdk -y
+VALIDATE $? "Installation of JAVA"
+
+#docker
+sudo dnf -y install dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo usermod -aG docker ec2-user
+sudo usermod -aG docker jenkins
+
+sudo systemctl start docker
+docker run hello-world
+
+docker --version
+VALIDATE $? "Docker installation"
+
+#Resize Disk
+lsblk
+
+growpart /dev/nvme0n1 4
+VALIDATE $? "Disk Partition"
+
+lvextend -l +50%FREE /dev/RootVG/rootVol 
+
+
+lvextend -l +50%FREE /dev/RootVG/varVol
+
+xfs_growfs /
+VALIDATE $? "Resize of RootVol"
+
+xfs_growfs /var
+VALIDATE $? "Resize of VarVol"
--- jenkins-agent-tf/jenkins-master.sh
+++ jenkins-agent-tf/jenkins-master.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is FAILED"
+        exit 1
+    else
+        echo "$2 is SUCCESS"
+     fi
+}
+
+CHECK(){
+    if [ $USER_ID -ne 0 ]
+    then 
+        echo "Please Run this scirpt with ROOT previleges"
+        exit 1
+    fi
+}
+CHECK
+
+#jenkins installation
 sudo curl -o /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
@@ -12,3 +34,4 @@ sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+VALIDATE $? "Jenkins Installation"
